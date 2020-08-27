dec5b099569ec08c95fba058b9c66100dc53fe14
KTarun003 <tarunreddykreddy@gmail.com>
2020-08-27 21:50:40 +0530

    Scaffolded Database

---
+++ StoreManager/Models/Items.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StoreManager.Models
+{
+    public partial class Items
+    {
+        public string ItemId { get; set; }
+        public int BillId { get; set; }
+
+        public virtual Bills Bill { get; set; }
+        public virtual Inventory Item { get; set; }
+    }
+}
---
+++ StoreManager/Models/ItemsCost.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StoreManager.Models
+{
+    public partial class ItemsCost
+    {
+        public string Id { get; set; }
+        public float? Cost { get; set; }
+
+        public virtual Inventory IdNavigation { get; set; }
+    }
+}
