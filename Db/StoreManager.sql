7fec4e1ec4cf9a2edec0c9f3f6b683985267059d
KTarun003 <tarunreddykreddy@gmail.com>
2020-09-03 11:41:28 +0530

    Sql Dump

---
+++ Db/StoreManager.sql
@@ -0,0 +1,179 @@
+CREATE DATABASE  IF NOT EXISTS `store_manager` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `store_manager`;
+-- MySQL dump 10.13  Distrib 8.0.20, for Win64 (x86_64)
+--
+-- Host: localhost    Database: store_manager
+-- ------------------------------------------------------
+-- Server version	8.0.20
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `bills`
+--
+
+DROP TABLE IF EXISTS `bills`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `bills` (
+  `id` int NOT NULL,
+  `name` varchar(45) DEFAULT NULL,
+  `amount` float DEFAULT NULL,
+  `quantity` int DEFAULT NULL,
+  `location` varchar(45) DEFAULT 'home',
+  `bill_date` date DEFAULT (curdate()),
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `bills`
+--
+
+LOCK TABLES `bills` WRITE;
+/*!40000 ALTER TABLE `bills` DISABLE KEYS */;
+INSERT INTO `bills` VALUES (1,'Varun',1000,2,'home','2020-08-28'),(2,'Sandra',2000,2,'store','2020-09-02'),(3,'Sravan',3000,2,'home','2020-09-02');
+/*!40000 ALTER TABLE `bills` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `dashboard`
+--
+
+DROP TABLE IF EXISTS `dashboard`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `dashboard` (
+  `id` int NOT NULL,
+  `total_items` int DEFAULT NULL,
+  `total_worth` float DEFAULT NULL,
+  `total_bills` int DEFAULT NULL,
+  `total_amount` float DEFAULT NULL,
+  `monthly_bills` int DEFAULT NULL,
+  `monthly_amount` float DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `dashboard`
+--
+
+LOCK TABLES `dashboard` WRITE;
+/*!40000 ALTER TABLE `dashboard` DISABLE KEYS */;
+INSERT INTO `dashboard` VALUES (1,2,1390,3,6000,2,5000);
+/*!40000 ALTER TABLE `dashboard` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `inventory`
+--
+
+DROP TABLE IF EXISTS `inventory`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `inventory` (
+  `id` varchar(45) NOT NULL,
+  `name` varchar(45) DEFAULT NULL,
+  `type` varchar(45) DEFAULT NULL,
+  `size` varchar(45) DEFAULT NULL,
+  `cost` float DEFAULT NULL,
+  `price` float DEFAULT NULL,
+  `quantity` int DEFAULT NULL,
+  `location` varchar(45) DEFAULT 'home',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `inventory`
+--
+
+LOCK TABLES `inventory` WRITE;
+/*!40000 ALTER TABLE `inventory` DISABLE KEYS */;
+INSERT INTO `inventory` VALUES ('1','Cashew','Packed','1 Kg',NULL,670,4,'home'),('2','Almonds','Packed','1 Kg',NULL,720,4,'home');
+/*!40000 ALTER TABLE `inventory` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `items`
+--
+
+DROP TABLE IF EXISTS `items`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `items` (
+  `itemId` varchar(45) NOT NULL,
+  `billId` int NOT NULL,
+  PRIMARY KEY (`itemId`,`billId`),
+  KEY `bill_idx` (`billId`),
+  CONSTRAINT `bill0` FOREIGN KEY (`billId`) REFERENCES `bills` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `inventory0` FOREIGN KEY (`itemId`) REFERENCES `inventory` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `items`
+--
+
+LOCK TABLES `items` WRITE;
+/*!40000 ALTER TABLE `items` DISABLE KEYS */;
+INSERT INTO `items` VALUES ('1',1),('2',1),('1',2),('2',2),('1',3),('2',3);
+/*!40000 ALTER TABLE `items` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping routines for database 'store_manager'
+--
+/*!50003 DROP PROCEDURE IF EXISTS `UpdateDashboard` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`Admin`@`%` PROCEDURE `UpdateDashboard`()
+BEGIN
+    DECLARE no_of_items,no_of_bills,month_bills,C INT;
+    DECLARE worth,bill_amount,month_amount FLOAT;
+    SELECT COUNT(*) INTO no_of_items from inventory;
+    SELECT COUNT(*) INTO no_of_bills from bills;
+    SELECT COUNT(*) INTO month_bills from bills where MONTH(bill_date) = MONTH(CURRENT_DATE());
+    SELECT SUM(price) INTO worth from inventory;
+    SELECT SUM(amount) INTO bill_amount from bills;
+    SELECT SUM(amount) INTO month_amount from bills where MONTH(bill_date) = MONTH(CURRENT_DATE());
+    SELECT COUNT(*) INTO C from dashboard;
+    IF C = 0 THEN
+        INSERT INTO dashboard values (1,no_of_items,worth,no_of_bills,bill_amount,month_bills,month_amount);
+    ELSE
+        UPDATE dashboard set total_items = no_of_items ,total_worth = worth ,total_bills = no_of_bills ,total_amount = bill_amount ,monthly_bills = month_bills ,monthly_amount = month_amount where id=1;
+    end if;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-09-03 11:40:08
